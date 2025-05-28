76b740a35225bedd84845aff1c2ec09b0c08e4f5
Antsibor Daniil <s1lence0073777@gmail.com>
2025-05-29 01:26:06 +0300

    profile edited

--- frontend/src/components/ProfileContainer/ProfileContainer.tsx
+++ frontend/src/components/ProfileContainer/ProfileContainer.tsx
@@ -12,22 +12,73 @@ export default function ProfileContainer() {
             <h1 className={styles.title}>Параметры профиля</h1>
             <div className={styles.containerForInfo}>
                 <h2>Фото профиля</h2>
-                <div className={styles.blockWithAvatar}>
-                    <div className={styles.profilePhoto}>
-                        <img
-                            src="/images/Avatar.png"
-                            alt="Фото профиля"
-                            className={styles.photo}
-                        />
-                        <div className={styles.btns}>
-                            <button className={styles.firstButton}>
-                                <img src="/images/ImageUpload.png" alt="" />
-                                Загрузить
-                            </button>
-                            <button className={styles.secondButton}>
-                                <img src="/images/Trash.png" alt="" />
-                                Удалить
-                            </button>
+                <div className={styles.rowWith}>
+                    <div className={styles.blockWithAvatar}>
+                        <div className={styles.profilePhoto}>
+                            <img
+                                src="/images/Avatar.png"
+                                alt="Фото профиля"
+                                className={styles.photo}
+                            />
+                            <div className={styles.btns}>
+                                <button className={styles.firstButton}>
+                                    <img src="/images/ImageUpload.png" alt="" />
+                                    Загрузить
+                                </button>
+                                <button className={styles.secondButton}>
+                                    <img src="/images/Trash.png" alt="" />
+                                    Удалить
+                                </button>
+                            </div>
+                        </div>
+                        <div className={styles.personalData}>
+                            <h2>Личные данные</h2>
+                            <div className={styles.containerForInputs}>
+                                <label>
+                                    Полное имя
+                                    <input
+                                        type="text"
+                                        value={fullName}
+                                        onChange={(e) =>
+                                            setFullName(e.target.value)
+                                        }
+                                        placeholder="Имя"
+                                    />
+                                </label>
+                                <label>
+                                    Публичное имя
+                                    <input
+                                        type="text"
+                                        value={publicName}
+                                        onChange={(e) =>
+                                            setPublicName(e.target.value)
+                                        }
+                                        placeholder="Публичное имя"
+                                    />
+                                </label>
+                                <label>
+                                    Почта
+                                    <input
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                        placeholder="mail@mail.mail"
+                                    />
+                                </label>
+                                <label>
+                                    Телефон
+                                    <input
+                                        type="text"
+                                        value={phone}
+                                        onChange={(e) =>
+                                            setPhone(e.target.value)
+                                        }
+                                        placeholder="+99999999999"
+                                    />
+                                </label>
+                            </div>
                         </div>
                     </div>
                     <div className={styles.changesButton}>
@@ -37,50 +88,6 @@ export default function ProfileContainer() {
                             <button className={styles.second}>Отменить</button>
                         </div>
                     </div>
-                </div>
-
-                <div className={styles.personalData}>
-                    <h2>Личные данные</h2>
-                    <div className={styles.containerForInputs}>
-                        <label>
-                            Полное имя
-                            <input
-                                type="text"
-                                value={fullName}
-                                onChange={(e) => setFullName(e.target.value)}
-                                placeholder="Имя"
-                            />
-                        </label>
-                        <label>
-                            Публичное имя
-                            <input
-                                type="text"
-                                value={publicName}
-                                onChange={(e) => setPublicName(e.target.value)}
-                                placeholder="Публичное имя"
-                            />
-                        </label>
-                        <label>
-                            Почта
-                            <input
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder="mail@mail.mail"
-                            />
-                        </label>
-                        <label>
-                            Телефон
-                            <input
-                                type="text"
-                                value={phone}
-                                onChange={(e) => setPhone(e.target.value)}
-                                placeholder="+99999999999"
-                            />
-                        </label>
-                    </div>
-
-                    
                 </div>
             </div>
         </section>
